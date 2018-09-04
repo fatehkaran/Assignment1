141d9c65843f9982d25d1cdc2b812b4b916ccf05
Fateh Karan Singh Sandhu <fatehkaran@huskers.unl.edu>
2018-09-03 20:01:13 -0500

    changes

--- rateOfReturn.c
+++ rateOfReturn.c
@@ -11,29 +11,31 @@
 
 int main (int argc, char **argv) {
 
-double initialValue, finalValue;
+double initialValue, finalValue; // declare intial and final value variables
 
-double years, weeks, days;
+double years, weeks, days; // initalize variables for years, weeks, days
 
-printf("Please enter Initial Investment: ");
+printf("Please enter Initial Investment: "); // prompt for initial value
 scanf ("%lf", &initialValue);
 
-printf("Please enter Final Value: ");
+printf("Please enter Final Value: "); // prompt for final value
 scanf ("%lf", &finalValue);
 
-printf("Please enter years: ");
+printf("Please enter years: "); // prompt for years
 scanf ("%lf", &years);
 
-printf("Please enter weeks: ");
+printf("Please enter weeks: "); // prompt for weeks
 scanf ("%lf", &weeks);
 
-printf("Please enter days: ");
+printf("Please enter days: "); // prompt for days
 scanf ("%lf", &days);
 
 if ( initialValue < 0 || finalValue < 0 ) {
   printf ("Inital or Final Value cannot be less than zero\n");
   exit(1);
 }
+
+//error handling
 
 else if (initialValue > finalValue) {
   printf("Initial value cannot be greater than Final Value\n");
@@ -48,19 +50,19 @@ else if (weeks > 52 || days > 7) {
   exit(1);
 }
 else if (weeks == 52 && days!= 0){
-  printf("There cannot be more than zero days if weeks are 52\n");
+  printf("Days have to be 0 if weeks are 52\n");
   exit(1);
 }
 
-double t = ( (years*365)+(weeks*7)+(days) ) / 365;
+double t = ( (years*365)+(weeks*7)+(days) ) / 365; // convert years, weeks, days into time
 
-double FI = finalValue / initialValue;
+double FI = finalValue / initialValue; // final/initial
 
-double reciprocalT = 1 / t ;
+double reciprocalT = 1 / t ; // declare reciprocal
 
 double annualzedRateOfReturn;
 
-annualzedRateOfReturn = (( pow (FI, reciprocalT) ) - 1) * 100;
+annualzedRateOfReturn = (( pow (FI, reciprocalT) ) - 1) * 100; // formula
 
 printf ("Initial Value: $%.2f\n", initialValue);
 printf ("Final Value: $%.2f\n", finalValue);
